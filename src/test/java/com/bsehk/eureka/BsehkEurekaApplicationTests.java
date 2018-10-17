074a81d6978177033f60fc845627eefdf97891cb
wei <changminwei@synjones.net>
2018-10-17 09:17:34 +0800

    初始化

---
+++ src/test/java/com/bsehk/eureka/BsehkEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bsehk.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BsehkEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
